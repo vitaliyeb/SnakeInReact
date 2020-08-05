6bc95b4bb92948e942b746e15c44d8df0e0a0ce6
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-08-05 15:29:16 +0500

    сделал изменения оберток

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,56 @@
-import React from 'react';
-import { HorizontalIpad, VerticalalIpad, HorizontalIphone, VerticalalIphone } from '../display-wrappers/display-wrappers'
+import React from "react";
+import {
+  HorizontalIpad,
+  VerticalalIpad,
+  HorizontalIphone,
+  VerticalalIphone,
+} from "../display-wrappers/display-wrappers";
 
-export default class App extends React.Component{
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      wrapperName: this.setWrappedName(),
+    };
+  }
 
+  setWrappedName() {
+    let vw = document.documentElement.clientWidth;
+    let setName = null;
+    let wrapperName = null;
+    if (this.state) wrapperName = this.state.wrapperName;
 
+    if (vw > 1024) setName = "ipad-horizontal";
+    if (vw < 1024 && vw > 768) setName = "ipad-vertical";
+    if (vw < 768 && vw > 568) setName = "iphone-horizontal";
+    if (vw < 568) setName = "iphone-vertical";
 
-    render(){
-        return (
-            <VerticalalIphone>
-                <h2>children</h2>
-            </VerticalalIphone>
-        )
+    if (wrapperName != setName && this.state) return this.setState({ wrapperName: setName });
+    return setName;
+  }
+
+  componentDidMount() {
+    let maxSteep = 10;
+    let currentSteep = 0;
+    window.addEventListener("resize", (event) => {
+      if (currentSteep < maxSteep) return ++currentSteep;
+      currentSteep = 0;
+      this.setWrappedName();
+    });
+  }
+
+  render() {
+    let { wrapperName } = this.state;
+
+    switch (wrapperName){
+        case "ipad-horizontal":
+            return <HorizontalIpad><h2>children</h2></HorizontalIpad>
+        case "ipad-vertical":
+            return <VerticalalIpad><h2>children</h2></VerticalalIpad>
+        case "iphone-horizontal":
+            return <HorizontalIphone><h2>children</h2></HorizontalIphone>
+        case "iphone-vertical":    
+            return <VerticalalIphone><h2>children</h2></VerticalalIphone> 
+        }
     }
-} 
+}
