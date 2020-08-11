f49f6510a4f8b6f3d3e436c833aba5ef4f836b91
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-08-11 14:58:45 +0500

    first commit

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-# Тестовое задание для компании ООО Carprice.
+# Игра змейка написанная на React
 ---------------------------------------------
 
 
-Нужно было сделать подобие Ipad и Iphone, c возможностью переключения активных экранов, перетаскиванием иконок и открытием попапа по нажатию на иконку.
-
-![alt text](https://github.com//vitaliyeb/carPrice-test-tusk/blob/master/readmeImages/ih.png "Пример на ipad gorizontal")
-
-![alt text](https://github.com//vitaliyeb/carPrice-test-tusk/blob/master/readmeImages/iv.png "Пример на ipad vertical")
-
-![alt text](https://github.com//vitaliyeb/carPrice-test-tusk/blob/master/readmeImages/iphoneh.png "Пример на iphone gorizontal")
-
-![alt text](https://github.com//vitaliyeb/carPrice-test-tusk/blob/master/readmeImages/iphonev.png "Пример на iphone vertical")
--- src/index.js
+++ src/index.js
@@ -1,8 +1,63 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
-import App from './components/app/app'
+import React, {Suspense} from 'react';
+
+const ThemeContext = React.createContext('неизвестно');
+
+class NameUser extends React.Component {
+
+    render(){
+        let name = this.props.nameUser;
+
+        return(
+            <p>{name}</p>
+        )
+    }
+
+}
+
+
+export default class OtherComponent extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <p>Hello you name:</p>
+                <ThemeContext.Consumer>
+                    {(value)=> <NameUser nameUser={value} />}
+                </ThemeContext.Consumer>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+class Clock extends React.Component {
+
+    render(){
+        return (
+            <div>
+                <Suspense fallback={<div>Загрузка...</div>}>
+                    <ThemeContext.Provider value='Vitaliy'>
+                        <OtherComponent />
+                    </ThemeContext.Provider>
+                </Suspense>
+            </div>
+        )
+    }
+}
+
+
+
+
+
 
 ReactDOM.render(
-    <App />,
+    <Clock />,
     document.getElementById('root')
 )
+
+
+
